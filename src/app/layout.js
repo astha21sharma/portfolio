a13ddd138f224619766d92a7ad46a1352b8d3bf0
astha21sharma <b122028@iiit-bh.ac.in>
2024-08-16 18:40:05 +0530

    analytics

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,23 +1,24 @@
-import './globals.css'
-import { Inter } from 'next/font/google'
+import "./globals.css";
+import { Inter } from "next/font/google";
+import { Analytics } from "@vercel/analytics/react";
 
-
-
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: 'Portfolio - Astha  ❤️',
-  description: 'Created by Astha sharma ',
-}
+  title: "Portfolio - Astha  ❤️",
+  description: "Created by Astha sharma ",
+};
 
 export default function RootLayout({ children }) {
   return (
     <>
-
-    <html lang="en">
-        <link rel="icon" type="image/svg+xml" href='/logo-color.svg'></link>
-      <body className={inter.className}>{children}</body>
-    </html>
+      <html lang="en">
+        <link rel="icon" type="image/svg+xml" href="/logo-color.svg"></link>
+        <body className={inter.className}>
+          <Analytics />
+          {children}
+        </body>
+      </html>
     </>
-  )
+  );
 }
